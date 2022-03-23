641b8866353f12306b47f5527368fe380a495e99
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-03-23 12:40:30 -0300

    feat: start home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-export default function home(){
-    return (
-        <div>
-            <p>Hello Next.js</p>
-        </div>
-    )
+import { Box } from "@chakra-ui/react";
+
+export default function home() {
+  return (
+    <Box
+      w="100%"
+      height="100vh"
+      bgImage="url('/images/Bg.svg')"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      bgPosition="center"
+    ></Box>
+  );
 }
