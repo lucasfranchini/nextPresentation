716e154aee1028f026a32fffda5fd4bd9118edd5
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-03-23 12:45:03 -0300

    feat: add webvitals metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import type { AppProps, NextWebVitalsMetric } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
