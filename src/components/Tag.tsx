e65692606a6e8d9939f08f8e14f1e06ad2d500df
bruch0 <lucas.bruch0@gmail.com>
2022-03-24 16:23:34 -0300

    feat: add remove tag on clicking the button

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,17 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
 
 interface Props {
-  children: React.ReactNode;
+  children: string;
+  removeTag: Function;
+  tags: string[];
 }
 
-export default function Tag({ children }: Props) {
+export default function Tag({ children, removeTag, tags }: Props) {
   return (
     <Flex
       height="30px"
       border="0px"
       borderRadius="10px"
       position="relative"
-      p="5px 10px 25px 10px"
+      p="5px 40px 25px 10px"
       bg="#5232C9"
       m="5px"
       color="white"
@@ -20,6 +22,26 @@ export default function Tag({ children }: Props) {
       cursor="default"
     >
       {children}
+      <Flex
+        onClick={() => {
+          removeTag(tags.indexOf(children));
+        }}
+        position="absolute"
+        top="2.5px"
+        right="2.5px"
+        h="25px"
+        w="25px"
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="8px"
+        cursor="pointer"
+        color="white"
+        bg="#8c70f1"
+        transition="all .2s"
+        _hover={{ backgroundColor: "#b39fff" }}
+      >
+        x
+      </Flex>
     </Flex>
   );
 }
