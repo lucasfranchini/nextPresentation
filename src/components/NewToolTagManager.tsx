ea1d247c08f288f3c108f6aa3b4dbdbf0561b4ac
bruch0 <lucas.bruch0@gmail.com>
2022-03-24 15:45:36 -0300

    feat: add toast on error and news styles

--- src/components/NewToolTagManager.tsx
+++ src/components/NewToolTagManager.tsx
@@ -1,38 +1,55 @@
 import { KeyboardEvent, useState } from "react";
 import { Flex, Input } from "@chakra-ui/react";
 import Tool from "../interfaces/Tool";
+import Tag from "../components/Tag";
 
 type Props = {
   setNewTool: React.Dispatch<React.SetStateAction<any>>;
   newTool: Tool;
+  toast: Function;
 };
 
-export default function NewToolTagManager({ setNewTool, newTool }: Props) {
+export default function NewToolTagManager({
+  setNewTool,
+  newTool,
+  toast,
+}: Props) {
   const [newTag, setNewTag] = useState("");
 
   const addTag: Function = (tag: string) =>
     setNewTool({ ...newTool, tags: [...newTool.tags, tag] });
 
   const addOnEnter = (input: string) => {
-    if (input === "Enter") {
-      addTag(newTag);
-      setNewTag("");
+    if (input === "Enter" && newTag) {
+      if (newTool.tags.length > 10) {
+        toast({
+          description: "É possível adicionar no máximo 10 tags",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        addTag(newTag);
+        setNewTag("");
+      }
     }
   };
 
-  console.log(newTool);
   return (
     <Flex
       w="100%"
-      height="10vh"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="column"
+      height="fit-content"
+      minHeight="10vh"
       border="2px solid #EDF2F7"
-      borderRadius="15px"
+      borderRadius="15px 15px 0px 0px"
       position="relative"
       p="0"
     >
+      <Flex flexWrap="wrap">
+        {newTool.tags.map((tag) => (
+          <Tag>{tag}</Tag>
+        ))}
+      </Flex>
       <Input
         value={newTag}
         onChange={(e: React.FormEvent<HTMLInputElement>) =>
@@ -40,11 +57,17 @@ export default function NewToolTagManager({ setNewTool, newTool }: Props) {
         }
         onKeyUp={(e: KeyboardEvent) => addOnEnter(e.nativeEvent.code)}
         position="absolute"
-        bottom="0"
+        bottom="-48px"
+        left="-2px"
         size="lg"
+        width="calc(100% + 4px)"
+        outline="0px"
         variant="filled"
         focusBorderColor="#5232C9"
+        borderRadius="0px 0px 10px 10px"
+        border="2px solid #EDF2F7"
         m="0"
+        placeholder="Aperte Enter para adicionar uma tag"
       />
     </Flex>
   );
