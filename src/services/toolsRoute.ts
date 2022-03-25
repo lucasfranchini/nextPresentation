dd769b8c93a8c2c8e1f6659ad7ccd3233e644835
bruch0 <lucas.bruch0@gmail.com>
2022-03-25 11:49:37 -0300

    feat: add delete tool route

--- src/services/toolsRoute.ts
+++ src/services/toolsRoute.ts
@@ -4,7 +4,10 @@ import Tool from "../interfaces/Tool";
 const createTool = ({ title, link, description, tags }: Tool) =>
   axios.post("http://localhost:3000/tools", { title, link, description, tags });
 
+const deleteTool = (toolId: string | string[] | undefined) =>
+  axios.delete(`http://localhost:3000/tools/${toolId}`);
+
 const getTool = (toolId: string | string[] | undefined) =>
   axios.get(`http://localhost:3000/tools/${toolId}`);
 
-export { createTool, getTool };
+export { createTool, getTool, deleteTool };
