c35bef2fd26e73ab2e8264ddc7c4d4b39f096075
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-03-28 14:43:04 -0300

    remove: unnecessary context

--- src/interfaces/SearchToolBarOptionsContext.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, SetStateAction } from "react";
-import toolFieldType from "../types/toolFieldType";
-
-export default interface SearchToolBarOptionsContextInterface {
-  SearchToolBarOptions: toolFieldType;
-  setSearchToolBarOptions: Dispatch<SetStateAction<toolFieldType>>;
-}
