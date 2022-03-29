540a24bc2b876bd3542cfb52f263cf554e33a2f9
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-03-29 10:34:20 -0300

    refactor: use validateURL function to add new tool

--- src/services/validateURL.ts
+++ src/services/validateURL.ts
@@ -1,4 +1,4 @@
-export default function validURL(str: string) {
+export default function validateURL(str: string) {
   var pattern = new RegExp(
     "^(https?:\\/\\/)?" + // protocol
       "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
