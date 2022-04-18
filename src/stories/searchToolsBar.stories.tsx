fe50c121658284e7a40ba8b0841d8c67e08ee946
Lucas Franchini <lucasfranchini.a@gmail.com>
2022-04-18 17:39:12 -0300

    feat: input stories

---
+++ src/stories/searchToolsBar.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import SearchBar from "../components/searchToolsBar/Input";
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Layout/SearchBar",
+  component: SearchBar,
+} as ComponentMeta<typeof SearchBar>;
+
+const Template: ComponentStory<typeof SearchBar> = (args) => {
+  return <SearchBar {...args} />;
+};
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  //@ts-ignore
+  fontWeight: "400",
+  border: "1px",
+};
