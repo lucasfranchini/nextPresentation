75c641825e5f41c0b3c1a22160beaed4a89a6f1f
Lucas Franchini Alves <lucasfranchini.a@gmail.com>
2022-04-26 16:23:07 -0300

    Feature/docker configuration (#18)
    * feat: initial dockerFile configuration
    
    * feat: container for dev env
    
    * feat: docker-compose
    
    * feat: fix url for external requisitions
    
    * refactor: transform docker compose to dev env

--- src/services/toolsRoute.ts
+++ src/services/toolsRoute.ts
@@ -3,15 +3,22 @@ import Tool from "../interfaces/Tool";
 import ToolFieldType from "../types/toolFieldType";
 
 const createTool = ({ title, link, description, tags }: Tool) =>
-  axios.post("http://localhost:3000/tools", { title, link, description, tags });
+  axios.post(`${process.env.NEXT_PUBLIC_API_URL}/tools`, {
+    title,
+    link,
+    description,
+    tags,
+  });
 
 const deleteTool = (toolId: string | string[] | undefined) =>
-  axios.delete(`http://localhost:3000/tools/${toolId}`);
+  axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/tools/${toolId}`);
 
 const getTool = (toolId: string | string[] | undefined) =>
-  axios.get(`http://localhost:3000/tools/${toolId}`);
+  axios.get(`${process.env.NEXT_PUBLIC_API_URL}/tools/${toolId}`);
 
 const searchTools = async (searchText: string, field: ToolFieldType) =>
-  axios.post("http://localhost:3000/tools/search", { searchText, field });
-
+  axios.post(`${process.env.NEXT_PUBLIC_API_URL}/tools/search`, {
+    searchText,
+    field,
+  });
 export { createTool, getTool, deleteTool, searchTools };
